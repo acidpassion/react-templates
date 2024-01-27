148a91c30f392d26fa23092e70e45f2ff4e8e934
Alex <alex.xie.gm@gmail.com>
2024-01-27 17:42:28 +0800

    Update UserList.tsx

--- UserList.tsx
+++ UserList.tsx
@@ -1,59 +1,34 @@
-// components/UserList/UserList.js
+// components/UserList/UserList.tsx
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@material-ui/core';
-import UserFormDialog from './UserFormDialog';
-import { deleteUser } from '../../redux/actions/userActions';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button, DataGrid, GridColDef } from '@material-ui/data-grid';
+import { RootState } from '../../redux/store';
+import { selectUser, User } from '../../redux/actions/userActions';
 
-const UserList = () => {
-  const users = useSelector((state) => state.user.users);
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'name', headerName: 'Name', width: 150 },
+  { field: 'email', headerName: 'Email', width: 200 },
+];
+
+const UserList: React.FC = () => {
   const dispatch = useDispatch();
-  const [openDialog, setOpenDialog] = React.useState(false);
-  const [selectedUser, setSelectedUser] = React.useState(null);
+  const users = useSelector((state: RootState) => state.user.users);
 
-  const handleEditUser = (user) => {
-    setSelectedUser(user);
-    setOpenDialog(true);
-  };
-
-  const handleDeleteUser = (userId) => {
-    dispatch(deleteUser(userId));
-  };
-
-  const handleAddUser = () => {
-    setSelectedUser(null);
-    setOpenDialog(true);
-  };
-
-  const handleCloseDialog = () => {
-    setOpenDialog(false);
+  const handleRowClick = (params: { data: User }) => {
+    dispatch(selectUser(params.data));
   };
 
   return (
     <div>
-      <Button variant="contained" color="primary" onClick={handleAddUser}>
-        Add User
-      </Button>
-      {/* Render your user grid here with edit and delete buttons */}
-      {users.map((user) => (
-        <div key={user.id}>
-          <span>{user.name}</span>
-          <Button onClick={() => handleEditUser(user)}>Edit</Button>
-          <Button onClick={() => handleDeleteUser(user.id)}>Delete</Button>
-        </div>
-      ))}
-      {/* User Form Dialog */}
-      <Dialog open={openDialog} onClose={handleCloseDialog}>
-        <DialogTitle>{selectedUser ? 'Edit User' : 'Add User'}</DialogTitle>
-        <DialogContent>
-          <UserFormDialog user={selectedUser} onClose={handleCloseDialog} />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseDialog} color="primary">
-            Cancel
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <DataGrid
+        rows={users}
+        columns={columns}
+        pageSize={5}
+        autoHeight
+        onRowClick={handleRowClick}
+      />
+      <Button onClick={() => dispatch(selectUser(null))}>Create New User</Button>
     </div>
   );
 };
