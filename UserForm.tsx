13f64ad15713c7e7459f46be0d0c6865af92c665
Alex <alex.xie.gm@gmail.com>
2024-01-27 17:42:42 +0800

    Update UserForm.tsx

--- UserForm.tsx
+++ UserForm.tsx
@@ -1,30 +1,50 @@
-// components/UserList/UserFormDialog.js
-import React from 'react';
+// components/UserDetailsForm/UserDetailsForm.tsx
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { TextField, Button } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-import { addUser, editUser } from '../../redux/actions/userActions';
+import { RootState } from '../../redux/store';
+import { selectUser, User } from '../../redux/actions/userActions';
 
-const UserFormDialog = ({ user, onClose }) => {
+const UserDetailsForm: React.FC = () => {
   const dispatch = useDispatch();
-  const [name, setName] = React.useState(user ? user.name : '');
+  const selectedUser = useSelector((state: RootState) => state.user.selectedUser);
+  const [userDetails, setUserDetails] = useState<User | null>(null);
+
+  useEffect(() => {
+    setUserDetails(selectedUser);
+  }, [selectedUser]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!userDetails) return;
+    setUserDetails({ ...userDetails, [e.target.name]: e.target.value });
+  };
 
   const handleSave = () => {
-    const newUser = { id: user ? user.id : Date.now(), name };
-    if (user) {
-      dispatch(editUser(newUser));
-    } else {
-      dispatch(addUser(newUser));
-    }
-    onClose();
+    if (!userDetails) return;
+    // Dispatch action to save or update user details in the Redux store
+    // For now, let's just log the details to the console
+    console.log('Save User Details:', userDetails);
   };
 
   return (
     <div>
-      <TextField
-        label="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
+      <h2>User Details Form</h2>
+      {userDetails && (
+        <>
+          <TextField
+            name="name"
+            label="Name"
+            value={userDetails.name}
+            onChange={handleInputChange}
+          />
+          <TextField
+            name="email"
+            label="Email"
+            value={userDetails.email}
+            onChange={handleInputChange}
+          />
+        </>
+      )}
       <Button onClick={handleSave} color="primary">
         Save
       </Button>
@@ -32,4 +52,4 @@ const UserFormDialog = ({ user, onClose }) => {
   );
 };
 
-export default UserFormDialog;
+export default UserDetailsForm;
